f00cec238e762d44e72d3193bcd9fae4dea878f4
Rizky Hendra Saputra <71587872+RizkyHendra@users.noreply.github.com>
2022-03-15 13:19:28 +0700

    pushScore

--- Assets/Script/Player/Player.cs
+++ Assets/Script/Player/Player.cs
@@ -10,9 +10,16 @@ public class Player : MonoBehaviour
 
     private Rigidbody2D rig;
 
+    [Header("Scoring")]
+    public ScoreController score;
+    public float scoringRatio;
+    private float lastPositionX;
+
+
     private void Start()
     {
         rig = GetComponent<Rigidbody2D>();
+        lastPositionX = transform.position.x;
     }
     private void FixedUpdate()
     {
@@ -20,4 +27,18 @@ public class Player : MonoBehaviour
         velocityvector.x = Mathf.Clamp(velocityvector.x + moveAccel * Time.deltaTime, 0.0f, maxSpeed);
         rig.velocity = velocityvector;
     }
+    private void Update()
+    {
+        int distancePassed = Mathf.FloorToInt(transform.position.x - lastPositionX);
+        int scoreIncrement = Mathf.FloorToInt(distancePassed / scoringRatio);
+
+        if(scoreIncrement > 0)
+        {
+            score.IncreaseCurrentScore(scoreIncrement);
+            lastPositionX += distancePassed;
+        }
+    }
+   
+
+
 }
