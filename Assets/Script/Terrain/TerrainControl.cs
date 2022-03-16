fe57f959b0f5ac71b4312de933054a2e02eb3458
RizkyHendraSaputra <4end.studio@gmail.com>
2022-03-16 19:12:56 +0700

    PUSH TAP AND SLOWMO

--- Assets/Script/Terrain/TerrainControl.cs
+++ Assets/Script/Terrain/TerrainControl.cs
@@ -8,6 +8,6 @@ public class TerrainControl : MonoBehaviour
     private const float debugLineHeight = 10.0f;
     private void OnDrawGizmos()
     {
-        Debug.DrawLine(transform.position + Vector3.up * debugLineHeight / 2, transform.position + Vector3.down * debugLineHeight / 2, Color.green);
+        Debug.DrawLine(transform.position + new Vector3(6,2) * debugLineHeight / 2, transform.position + new Vector3(6,-2) * debugLineHeight / 2, Color.green);
     }
 }
--- Assets/Script/Tower/Bullet.cs
+++ Assets/Script/Tower/Bullet.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("SLOWMO")]
+    public float slowMotionTimeScale;
+    private float startTimeScale;
+    private float startFixedDeltaTime;
+
+
+    public GameObject shashEffect;
+   
     void Start()
     {
-        
+        startTimeScale = Time.timeScale;
+        startFixedDeltaTime = Time.fixedDeltaTime;
     }
 
     // Update is called once per frame
@@ -18,9 +26,43 @@ public class Bullet : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Destroy(this.gameObject);
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            startSlowMo();
+        }
+      
 
     }
-    
-    
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            stopSlowmo();
+        }
+    }
+    void startSlowMo()
+    {
+        Time.timeScale = slowMotionTimeScale;
+        Time.fixedDeltaTime = startFixedDeltaTime * slowMotionTimeScale;
+    }
+    void stopSlowmo()
+    {
+        Time.timeScale = startTimeScale;
+        Time.fixedDeltaTime = startFixedDeltaTime;
+    }
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+        Instantiate(shashEffect, transform.position, Quaternion.identity);
+       
+        
+    }
+
+
+
 }
